17a15664475676f867a813af286fff78a3b9c696
ali <amw223@cam.ac.uk>
2020-02-23 16:44:47 +0000

    Attempted to add textures to graphics application.

--- resources/textureVertexShader.glsl
+++ resources/textureVertexShader.glsl
@@ -2,22 +2,19 @@
 
 // Input vertex data, different for all executions of this shader.
 layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec2 vertexUV;
 
-// Notice that the "1" here equals the "1" in glVertexAttribPointer
-layout(location = 1) in vec3 vertexColor;
+// Output data ; will be interpolated for each fragment.
+out vec2 UV;
 
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
-
-// Output data ; will be interpolated for each fragment.
-out vec3 fragmentColor;
 
 void main(){
 
     // Output position of the vertex, in clip space : MVP * position
     gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
 
-    // The color of each vertex will be interpolated
-    // to produce the color of each fragment
-    fragmentColor = vertexColor;
+    // UV of the vertex. No special space for this one.
+    UV = vertexUV;
 }
--- src/uk/ac/cam/amw223/tinyPlanet/graphicsApplication.java
+++ src/uk/ac/cam/amw223/tinyPlanet/graphicsApplication.java
@@ -7,7 +7,9 @@ import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,6 +17,7 @@ import java.nio.file.Path;
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.GL_BGR;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
@@ -26,7 +29,87 @@ public class graphicsApplication {
     // The window handle
     private long window;
 
-    private int programID;
+
+    static float[] g_vertex_buffer_data = {
+            -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+            -1.0f,-1.0f, 1.0f,
+            -1.0f, 1.0f, 1.0f, // triangle 1 : end
+            1.0f, 1.0f,-1.0f, // triangle 2 : begin
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f, // triangle 2 : end
+            1.0f,-1.0f, 1.0f,// 3
+            -1.0f,-1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,//4
+            1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,//5
+            -1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f, 1.0f,//6
+            -1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,//7
+            -1.0f,-1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            -1.0f, 1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f
+    };
+
+
+
+    // One color for each vertex. They were generated randomly.
+    float[] g_color_buffer_data = {
+            0.583f,  0.771f,  0.014f,
+            0.609f,  0.115f,  0.436f,
+            0.327f,  0.483f,  0.844f,
+            0.822f,  0.569f,  0.201f,
+            0.435f,  0.602f,  0.223f,
+            0.310f,  0.747f,  0.185f,
+            0.597f,  0.770f,  0.761f,
+            0.559f,  0.436f,  0.730f,
+            0.359f,  0.583f,  0.152f,
+            0.483f,  0.596f,  0.789f,
+            0.559f,  0.861f,  0.639f,
+            0.195f,  0.548f,  0.859f,
+            0.014f,  0.184f,  0.576f,
+            0.771f,  0.328f,  0.970f,
+            0.406f,  0.615f,  0.116f,
+            0.676f,  0.977f,  0.133f,
+            0.971f,  0.572f,  0.833f,
+            0.140f,  0.616f,  0.489f,
+            0.997f,  0.513f,  0.064f,
+            0.945f,  0.719f,  0.592f,
+            0.543f,  0.021f,  0.978f,
+            0.279f,  0.317f,  0.505f,
+            0.167f,  0.620f,  0.077f,
+            0.347f,  0.857f,  0.137f,
+            0.055f,  0.953f,  0.042f,
+            0.714f,  0.505f,  0.345f,
+            0.783f,  0.290f,  0.734f,
+            0.722f,  0.645f,  0.174f,
+            0.302f,  0.455f,  0.848f,
+            0.225f,  0.587f,  0.040f,
+            0.517f,  0.713f,  0.338f,
+            0.053f,  0.959f,  0.120f,
+            0.393f,  0.621f,  0.362f,
+            0.673f,  0.211f,  0.457f,
+            0.820f,  0.883f,  0.371f,
+            0.982f,  0.099f,  0.879f
+    };
 
     private final int width = 500;
 
@@ -70,6 +153,7 @@ public class graphicsApplication {
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+
         // Create the window
         window = glfwCreateWindow(500, 580, "Hello World!", NULL, NULL);
         if ( window == NULL )
@@ -108,6 +192,7 @@ public class graphicsApplication {
         // Make the window visible
         glfwShowWindow(window);
 
+
     }
 
     private void loop() throws IOException {
@@ -118,10 +203,78 @@ public class graphicsApplication {
         // bindings available for use.
         GL.createCapabilities();
 
-        programID = LoadShaders( "resources/simpleVertexShader.glsl", "resources/simpleFragmentShader.glsl" );
+        int programID = LoadShaders("resources/textureVertexShader.glsl", "resources/textureFragmentShader.glsl");
+
+        g_color_buffer_data = new float [12*3*3];
+        for (int v = 0; v < 12*3 ; v++){
+            g_color_buffer_data[3*v+0] = v/36.0f;
+            g_color_buffer_data[3*v+1] = 0f;
+            g_color_buffer_data[3*v+2] = 0f;
+        }
+
+        g_vertex_buffer_data = new float[]{
+                // left face
+                -1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f,-1.0f,
+
+                // back face
+                1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+
+                1.0f, 1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f,-1.0f,
+
+                // bottom face
+                1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+
+                1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f,-1.0f,
+
+                // front face
+                -1.0f, 1.0f, 1.0f,
+                -1.0f,-1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+
+                // right face
+                1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f,-1.0f,
+
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+
+                // top face
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f,-1.0f,
+                -1.0f, 1.0f,-1.0f,
+
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f
+        };
+
+        // Enable depth test
+        glEnable(GL_DEPTH_TEST);
+        // Accept fragment if it closer to the camera than the former one
+        glDepthFunc(GL_LESS);
 
         // Set the clear color
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
         glUseProgram(programID);
 
@@ -140,7 +293,7 @@ public class graphicsApplication {
 
         // Camera matrix
             Matrix4f View = new Matrix4f().lookAt(
-                    new Vector3f(4,3,3), // Camera is at (4,3,3), in World Space
+                    new Vector3f(-4,3,3), // Camera is at (4,3,3), in World Space
                     new Vector3f(0,0,0), // and looks at the origin
                     new Vector3f(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
         );
@@ -158,6 +311,13 @@ public class graphicsApplication {
         // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
         glUniformMatrix4fv(MatrixID, false, mvp.get(new float[16]));// this new float[] bit could be a source of errors
 
+        float time = 0f;
+        float[] rgb = new float[3];
+        boolean running = true;
+        boolean toggle = false;//for debouncing?
+        int i;
+
+        int Texture = loadBMP_custom("resources/checkerboard.bmp");
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
@@ -171,18 +331,62 @@ public class graphicsApplication {
                 0.5f, -0.5f
             };
 
+
+
+            if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+                if (!toggle) {
+                    running = !running;
+                    toggle = true;
+                }
+            } else {
+                toggle = false;
+            }
+
+            for (int v = 0; v < 12*3 ; v++){
+                i = v/6 + 1;
+                g_color_buffer_data[3*v+0] = (float)Math.abs(Math.sin(time)) * (i/6f);
+                g_color_buffer_data[3*v+1] = (float)Math.abs(Math.sin(time + 2*Math.PI/3)) * (i/6f);
+                g_color_buffer_data[3*v+2] = (float)Math.abs(Math.sin(time + 4*Math.PI/3)) * (i/6f);
+            }
+            if (running) {
+                time += 0.02f;
+                if (time > Math.PI * 2) {
+                    time = 0f;
+                }
+            }
+
+            // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+            // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+
             int vao = glGenVertexArrays();
             glBindVertexArray(vao);
 
             int vbo = glGenBuffers();
             glBindBuffer(GL_ARRAY_BUFFER, vbo);
-            glBufferData(GL_ARRAY_BUFFER, verteces, GL_STATIC_DRAW);
-            glVertexAttribPointer(0, 2, GL_FLOAT, false,0, 0);
+            //glBufferData(GL_ARRAY_BUFFER, verteces, GL_STATIC_DRAW);
+            glBufferData(GL_ARRAY_BUFFER, g_vertex_buffer_data, GL_STATIC_DRAW);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false,0, 0);
             glEnableVertexAttribArray(0);
 
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-            // OMG I DID IT!!
 
+            int colorBuffer = glGenBuffers();
+            glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+            glBufferData(GL_ARRAY_BUFFER, g_color_buffer_data, GL_STATIC_DRAW);
+            // 2nd attribute buffer : colors
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+            glVertexAttribPointer(
+                    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+                    3,                                // size
+                    GL_FLOAT,                         // type
+                    false,                         // normalized?
+                    0,                                // stride
+                    0                          // array buffer offset
+            );
+
+
+            glDrawArrays(GL_TRIANGLES, 0, 12*3);
+            // OMG I DID IT!!
 
 
             glfwSwapBuffers(window); // swap the color buffers
@@ -253,6 +457,77 @@ public class graphicsApplication {
         glDeleteShader(VertexShaderID);
         glDeleteShader(FragmentShaderID);
         return ProgramID;
+    }
+
+    private int loadBMP_custom(String imagePath) {
+        // Data read from the header of the BMP file
+        byte[] header = new byte[54]; // Each BMP file begins by a 54-bytes header
+        int dataPos;     // Position in the file where the actual data begins
+        int width = 0, height = 0;
+        int imageSize;   // = width*height*3
+        // Actual RGB data
+        byte[] data;
+        ByteBuffer buf = null;
+
+        InputStream is = null;
+        try {
+            // new input stream created
+            is = new FileInputStream(imagePath);
+
+            // read stream data into buffer
+            if (is.read(header) != 54) {
+                System.out.println("Not a correct BMP file");
+                return -1;
+            }
+            if ((char)header[0] != 'B' || (char)header[1] != 'M') {
+                System.out.println("Not a correct BMP file");
+                return -1;
+            }
+
+            // Read ints from the byte array
+            dataPos = header[0x0A];
+            imageSize = header[0x22];
+            width = header[0x12];
+            height = header[0x16];
+
+            // Some BMP files are misformatted, guess missing information
+            if (imageSize == 0) {
+                imageSize = width*height*3; // 3 : one byte for each Red, Green and Blue component
+            }
+            if (dataPos == 0) {
+                dataPos = 54; // The BMP header is done that way
+            }
+
+            // Create a buffer
+            data = new byte[imageSize];
+
+            // Read the actual data from the file into the buffer
+            if (is.read(data,1, imageSize) < imageSize) {
+                System.out.println("Error reading image. Not enough bytes read.");
+            }
+
+            // Wrap a byte array into a buffer
+            buf = ByteBuffer.wrap(data);
+
+            is.close();
+        } catch(IOException e) {
+            // if any I/O error occurs
+            e.printStackTrace();
+        }
+
+        int textureID;
+        textureID = glGenTextures();
+
+        // "Bind" the newly created texture : all future texture functions will modify this texture
+        glBindTexture(GL_TEXTURE_2D, textureID);
+
+        // Give the image to OpenGL
+        glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, buf);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+        return textureID;
     }
 
     public static void main(String[] args) {
